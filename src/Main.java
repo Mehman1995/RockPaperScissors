1573f3432d5b9800954dc170c242986587d9ad1e
Mehman <mixailguliev@mail.ru>
2023-03-11 08:56:06 +0300

    Небольшое развлечение

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+         Player bot = new Player();
+         Player Mehman = new Player("Mehman", RPS.SCISSORS);
+        System.out.println(bot.whoWins(Mehman,bot));
     }
 }
